26a87cf7239f36292dd5654491fc3577516e291b
Syalwa Naura Rizquna <syalwanana123@gmail.com>
2021-11-15 08:51:43 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,18 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$nama_database = "dbperpus"; //nama database
+
+$db = mysqli_connect(
+    $server,
+    $user,
+    $password,
+    $nama_database
+);//mengkoneksikan dengan database
+
+    // if($db){
+    //     echo "koneksi berhasil";
+    // }else{
+    //     echo "koneksi gagal";
+    // }
---
+++ pages/beranda.php
@@ -0,0 +1,13 @@
+<div id="label-page">
+	<h3>Beranda</h3>
+</div>
+<div id="content">
+	<div id="beranda-judul">
+		<h1>SELAMAT DATANG DI SISTEM INFORMASI PERPUSTAKAAN</h1>
+	</div>
+	<div id="beranda-konten">
+		<h2> "MEMBACA ADALAH JENDELA DUNIA" </h2>
+		<img src="https://objectstorage.ap-sydney-1.oraclecloud.com/n/sdpona2dqunm/b/perpus/o/Beranda.jpg" alt="Buku" style="width:500px;height:400px;">
+		<h4> &copy; SYALWA NAURA RIZQUNA </h4>
+	</div>
+</div>
